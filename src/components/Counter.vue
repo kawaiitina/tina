fd0df2b09b6e9e0ee340c0eb0ce7fea3d954b955
tina <qwertqw112@gmail.com>
2018-08-29 15:59:51 +0900

    init vue

---
+++ src/components/Counter.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-card>
+    <v-text-field v-model="count" type="number" label="Current Number"/>
+    <v-btn @click="add(input)">+</v-btn>
+    <v-btn @click="subtract(input)">-</v-btn>
+    <v-btn @click="multiply(input)">*</v-btn>
+    <v-btn @click="divide(input)">/</v-btn>
+    <v-text-field v-model="input" type="number" label="input"/>
+    <v-slider
+    v-model="input"
+    :max="10000"
+    :min="-10000"
+    :step="0.01"
+    label="input"
+    thumb-label        
+    ></v-slider>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data(){
+      return{
+          count: 0,
+          input: 0,
+          text: ''
+      }
+  },
+  methods:{
+      add(input){
+          this.count += input;
+      },
+      subtract(input){
+          this.count -= input;
+      },
+      multiply(input){
+          this.count *= input;
+      },
+      divide(input){
+          this.count /= input;
+      }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+</style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-card>
+    <v-form >
+        <v-text-field 
+            v-model="input" 
+            type="text" 
+            label="What to do?"
+            @keypress.enter="addTodo($event)"
+        />
+
+    </v-form>
+    <v-btn @click="addTodo">submit</v-btn>
+    <v-list>
+        <v-list-tile 
+        v-for="(todo, i) in todos"
+        :key="todo.date"
+        @click="deleteTodo(i)"
+        >
+        <v-list-tile-content>
+            <v-list-tile-title v-text="(i + 1) + '. ' + todo.text"/>
+        </v-list-tile-content>
+        </v-list-tile>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  data(){
+      return{
+          input: '',
+          todos: [
+              {
+                  text: "play Closers",
+                  date: Date.now()
+              },
+              {
+                  text: "delete Closers",
+                  date: Date.now() + 1 
+              }
+            ]
+      }
+  },
+  methods:{
+      addTodo(e){
+          e.preventDefault();
+          this.todos.push({
+              text: this.input,
+              date: Date.now()
+          });
+          this.input = '';
+      },
+      deleteTodo(i){
+          this.todos.splice(i,1);
+      }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+</style>
